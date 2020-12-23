a2a6a5f0c5ed207f0a2b584bfdbcc5a1f6c7d10c
MiguelED <migueled2695@gmail.com>
2020-12-23 02:03:16 -0600

    Se agrega jumbotron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './components/app/app.component';
 
 import { Appnavbar } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { JumboComponent } from './components/body/jumbo/jumbo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Appnavbar,
-    FooterComponent
+    FooterComponent,    
+    JumboComponent
   ],
   imports: [
     BrowserModule    
